0a51d1db4fcbb27d4f41d047c3367cef2dcf3725
Krupa78 <dankharakb78@gmail.com>
2018-03-14 12:47:50 +0530

    added genral

--- ProjectManagement/Dialogs/GeneralDialog.cs
+++ ProjectManagement/Dialogs/GeneralDialog.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,10 @@ namespace ProjectManagement.Dialogs
     [LuisModel("75a5d7cb-a133-4c5b-9f1e-1629d7c9acb9", "1d85b742f01843d086962485f0728cd5")]
     public class GeneralDialog
     {
-        /*public async Task StartAsync(IDialogContext context)
+        [LuisIntent("GoodMorning")]
+        public async Task GoodMorning(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("BOTTTT");
-            context.Wait(CheckGreetings);
+
         }
-
-        public async Task CheckGreetings(IDialogContext context, IAwaitable<object> result)
-        {
-            var message = await result;
-
-            if (message.Equals("Hello"))
-            {
-                await context.PostAsync("Hello, How can I help you?");
-            }
-        }*/
     }
 }
